4280d06d7043feef1ddb239b231393d205b36e44
Jarek Sliz <jarek@builderdesigns.com>
2018-04-10 22:15:20 -0500

    working on the header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,20 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
+import Scrollchor from 'react-scrollchor';
+
+import './styles.css';
 
 const Header = () => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
+      <Link to="/about" activeClassName="active">Services</Link>
+      <Link to="/about" activeClassName="active">Work</Link>
+      <Link to="/about" activeClassName="active">Skills</Link>
+      <Scrollchor to="#section03">Sample 3</Scrollchor>
+      <Scrollchor to="#section01">Sample 1</Scrollchor>
+      <Scrollchor to="#section02">Sample 2</Scrollchor>
     </nav>
   );
 };
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
+import Scrollchor from 'react-scrollchor';
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -27,6 +28,7 @@ class HomePage extends React.Component {
         <div className="jumbotron">
           <h1>Here is my First Bookstore</h1>
           <p>Here is a few information about a new react and redux bookstore</p>
+          <Scrollchor to="#section03">Sample</Scrollchor>
           <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
         </div>
         <div className="flexcontainer">
@@ -34,6 +36,28 @@ class HomePage extends React.Component {
           <h3>{this.state.number}</h3>
           <i className="btn btn-primary" onClick={this.IncrementNumber}>+</i>
         </div>
+        <div id="section03">
+          <p>03 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada luctus tellus at consectetur. Cras ut sodales risus. Donec fermentum enim in leo tristique viverra. Phasellus vitae blandit est. Maecenas vulputate lacinia odio in rhoncus. Integer orci lacus, volutpat eu tristique ut, pretium vel arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin ut lacus interdum, accumsan elit non, volutpat urna. Sed bibendum, sem id lobortis accumsan, tellus risus tincidunt ligula, at rhoncus turpis nunc sit amet felis. Donec quis ornare felis. Vestibulum eget varius orci. Mauris in convallis sapien, quis semper orci. Sed mollis faucibus tempus. Suspendisse aliquam mollis sapien, at auctor quam vehicula vel. Sed ornare vulputate leo sit amet pellentesque. Donec bibendum et nisi vitae imperdiet.</p>
+
+          <p>Mauris varius velit in neque vestibulum consequat. Sed consequat sodales arcu at volutpat. Nunc ornare arcu vel blandit tincidunt. Fusce libero diam, laoreet vel venenatis vitae, sollicitudin non nunc. Pellentesque et dolor pharetra, eleifend neque vel, finibus nulla. Vivamus sagittis velit eget posuere volutpat. Donec quam justo, rutrum quis rhoncus sit amet, mattis eu mi. Nulla facilisi. Nunc eros magna, lacinia a lorem a, aliquam lobortis ipsum. Proin sit amet sodales ante, vitae interdum risus. Nullam hendrerit leo nec maximus convallis.</p>
+
+          <p>Cras ultrices, nunc non condimentum vehicula, urna purus tincidunt felis, iaculis sodales risus arcu mattis quam. Donec et nisl congue, iaculis leo vitae, commodo elit. Morbi et mattis magna, id egestas lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nulla tellus, aliquam at dui quis, venenatis ullamcorper mi. Pellentesque ut mi hendrerit, sagittis ante blandit, posuere nisl. Sed non mi nec lacus dignissim fringilla. Praesent ullamcorper vitae arcu elementum maximus. Etiam volutpat condimentum metus sed faucibus. Donec enim urna, pellentesque sit amet urna ut, sollicitudin facilisis erat. Vestibulum scelerisque non velit molestie imperdiet. Fusce consequat eros eget dolor pretium fringilla. Quisque pellentesque eget neque nec sodales. Sed sit amet auctor enim, sed sodales urna. Maecenas eu eleifend diam.</p>
+        </div>
+        <div id="section02">
+          <p>02 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada luctus tellus at consectetur. Cras ut sodales risus. Donec fermentum enim in leo tristique viverra. Phasellus vitae blandit est. Maecenas vulputate lacinia odio in rhoncus. Integer orci lacus, volutpat eu tristique ut, pretium vel arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin ut lacus interdum, accumsan elit non, volutpat urna. Sed bibendum, sem id lobortis accumsan, tellus risus tincidunt ligula, at rhoncus turpis nunc sit amet felis. Donec quis ornare felis. Vestibulum eget varius orci. Mauris in convallis sapien, quis semper orci. Sed mollis faucibus tempus. Suspendisse aliquam mollis sapien, at auctor quam vehicula vel. Sed ornare vulputate leo sit amet pellentesque. Donec bibendum et nisi vitae imperdiet.</p>
+
+          <p>Mauris varius velit in neque vestibulum consequat. Sed consequat sodales arcu at volutpat. Nunc ornare arcu vel blandit tincidunt. Fusce libero diam, laoreet vel venenatis vitae, sollicitudin non nunc. Pellentesque et dolor pharetra, eleifend neque vel, finibus nulla. Vivamus sagittis velit eget posuere volutpat. Donec quam justo, rutrum quis rhoncus sit amet, mattis eu mi. Nulla facilisi. Nunc eros magna, lacinia a lorem a, aliquam lobortis ipsum. Proin sit amet sodales ante, vitae interdum risus. Nullam hendrerit leo nec maximus convallis.</p>
+
+          <p>Cras ultrices, nunc non condimentum vehicula, urna purus tincidunt felis, iaculis sodales risus arcu mattis quam. Donec et nisl congue, iaculis leo vitae, commodo elit. Morbi et mattis magna, id egestas lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nulla tellus, aliquam at dui quis, venenatis ullamcorper mi. Pellentesque ut mi hendrerit, sagittis ante blandit, posuere nisl. Sed non mi nec lacus dignissim fringilla. Praesent ullamcorper vitae arcu elementum maximus. Etiam volutpat condimentum metus sed faucibus. Donec enim urna, pellentesque sit amet urna ut, sollicitudin facilisis erat. Vestibulum scelerisque non velit molestie imperdiet. Fusce consequat eros eget dolor pretium fringilla. Quisque pellentesque eget neque nec sodales. Sed sit amet auctor enim, sed sodales urna. Maecenas eu eleifend diam.</p>
+        </div>
+        <div id="section01">
+          <p>01 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada luctus tellus at consectetur. Cras ut sodales risus. Donec fermentum enim in leo tristique viverra. Phasellus vitae blandit est. Maecenas vulputate lacinia odio in rhoncus. Integer orci lacus, volutpat eu tristique ut, pretium vel arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin ut lacus interdum, accumsan elit non, volutpat urna. Sed bibendum, sem id lobortis accumsan, tellus risus tincidunt ligula, at rhoncus turpis nunc sit amet felis. Donec quis ornare felis. Vestibulum eget varius orci. Mauris in convallis sapien, quis semper orci. Sed mollis faucibus tempus. Suspendisse aliquam mollis sapien, at auctor quam vehicula vel. Sed ornare vulputate leo sit amet pellentesque. Donec bibendum et nisi vitae imperdiet.</p>
+
+          <p>Mauris varius velit in neque vestibulum consequat. Sed consequat sodales arcu at volutpat. Nunc ornare arcu vel blandit tincidunt. Fusce libero diam, laoreet vel venenatis vitae, sollicitudin non nunc. Pellentesque et dolor pharetra, eleifend neque vel, finibus nulla. Vivamus sagittis velit eget posuere volutpat. Donec quam justo, rutrum quis rhoncus sit amet, mattis eu mi. Nulla facilisi. Nunc eros magna, lacinia a lorem a, aliquam lobortis ipsum. Proin sit amet sodales ante, vitae interdum risus. Nullam hendrerit leo nec maximus convallis.</p>
+
+          <p>Cras ultrices, nunc non condimentum vehicula, urna purus tincidunt felis, iaculis sodales risus arcu mattis quam. Donec et nisl congue, iaculis leo vitae, commodo elit. Morbi et mattis magna, id egestas lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nulla tellus, aliquam at dui quis, venenatis ullamcorper mi. Pellentesque ut mi hendrerit, sagittis ante blandit, posuere nisl. Sed non mi nec lacus dignissim fringilla. Praesent ullamcorper vitae arcu elementum maximus. Etiam volutpat condimentum metus sed faucibus. Donec enim urna, pellentesque sit amet urna ut, sollicitudin facilisis erat. Vestibulum scelerisque non velit molestie imperdiet. Fusce consequat eros eget dolor pretium fringilla. Quisque pellentesque eget neque nec sodales. Sed sit amet auctor enim, sed sodales urna. Maecenas eu eleifend diam.</p>
+        </div>
+
       </div>
     );
   }
