ff19060c95dabba5c7fb3dcd129c6f6b2d1968fe
Jarek Sliz <jarek@builderdesigns.com>
2018-04-19 23:10:25 -0500

    working on the header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,22 +1,31 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link, IndexLink } from 'react-router';
 import Scrollchor from 'react-scrollchor';
+import logo from '../../images/logo.png';
 
 import './styles.css';
 
 const Header = () => {
   return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/about" activeClassName="active">Services</Link>
-      <Link to="/about" activeClassName="active">Work</Link>
-      <Link to="/about" activeClassName="active">Skills</Link>
-      <Scrollchor to="#section03">Sample 3</Scrollchor>
-      <Scrollchor to="#section01">Sample 1</Scrollchor>
-      <Scrollchor to="#section02">Sample 2</Scrollchor>
+    <nav className="nav-wrapper">
+      <IndexLink to="/" activeClassName="active">
+        <img src={logo} />
+      </IndexLink>
+      <div className="links-wrapper">
+        <Link to="/about" activeClassName="active">Services</Link>
+        <Link to="/about" activeClassName="active">Work</Link>
+        <Link to="/about" activeClassName="active">Skills</Link>
+        <Scrollchor to="#section03">Sample 3</Scrollchor>
+        <Scrollchor to="#section01">Sample 1</Scrollchor>
+        <Scrollchor to="#section02">Sample 2</Scrollchor>
+      </div>
     </nav>
   );
+};
+
+Header.propTypes = {
+  myProp: PropTypes.bool
 };
 
 export default Header;
